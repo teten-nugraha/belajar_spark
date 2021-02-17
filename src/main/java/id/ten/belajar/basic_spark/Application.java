461c8116692bd1b633b643e6f74d8858d20edce5
Teten Nugraha <teten.nugraha@allianz.co.id>
2021-02-17 08:54:57 +0700

    simple spark and save to mysql

--- src/main/java/id/ten/belajar/basic_spark/Application.java
+++ src/main/java/id/ten/belajar/basic_spark/Application.java
@@ -2,7 +2,13 @@ package id.ten.belajar.basic_spark;
 
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SaveMode;
 import org.apache.spark.sql.SparkSession;
+
+import java.util.Properties;
+
+import static org.apache.spark.sql.functions.concat;
+import static org.apache.spark.sql.functions.lit;
 
 public class Application {
     public static void main(String[] args) {
@@ -18,7 +24,24 @@ public class Application {
                 .option("header",true)
                 .load("src/main/resources/name_and_comments.txt");
 
-        df.show(3);
+//        df.show(3);
 
+        // transformation
+        df = df.withColumn("full_name", concat(df.col("last_name"), lit(", "), df.col("first_name")));
+
+        // transformation
+        df = df.filter(df.col("comment").rlike("\\d+"))
+                .orderBy(df.col("last_name"));
+        df.show();
+
+        String dbConnUrl = "jdbc:mysql://localhost:3306/belajar_spark";
+        Properties prop = new Properties();
+        prop.setProperty("driver", "com.mysql.jdbc.Driver");
+        prop.setProperty("user", "root");
+        prop.setProperty("password", "root");
+
+        df.write()
+                .mode(SaveMode.Overwrite)
+                .jdbc(dbConnUrl, "project1", prop);
     }
 }
